2e6b1cf7b0d1804ef66a6c3631d39ac5712a6938
허태규 <taegyu-heo@naver.com>
2025-09-11 20:11:56 +0900

    Update App.java

--- HELLOJAVA/src/App.java
+++ HELLOJAVA/src/App.java
@@ -10,5 +10,25 @@ public class App {
         }
         System.out.println();
     }
+    public static void loopWithException(){
+        int divisor[] = {1, 2, 3, 4, 0};
+        for (int cnt = 0; cnt < 10; cnt++){
+            try{
+                int share = 100 / divisor[cnt];
+                System.out.println(share);
+            }
+            catch(java.lang.ArithmeticException e){
+                System.out.println("wrong");
+            }
+            catch(java.lang.ArrayOutOfBoundsException e){
+                System.out.println("wrong wrong");
+            }
+            finally{
+                System.out.println("final");
+            }
+        }
+        System.out.println("Done");
+    }
 }
+
 
