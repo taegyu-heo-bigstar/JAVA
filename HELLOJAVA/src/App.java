6851068b63234a2cf80e9ee95a211b563d0aa1da
허태규 <taegyu-heo@naver.com>
2025-09-11 19:41:37 +0900

    Update App.java

--- HELLOJAVA/src/App.java
+++ HELLOJAVA/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        printCharacter('*', 30);
+        printCharacter('-', 30);
+    }
+    public static void printCharcater(char ch, int num){
+        for (int cnt = 0; cnt < num; cnt++){
+            System.out.print(ch);
+        }
+        System.out.println();
     }
 }
+
